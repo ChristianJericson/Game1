47c662793c5b0a9974222035d03d15d9a03589b1
ChristianJericson <71628482+ChristianJericson@users.noreply.github.com>
2022-01-28 17:27:32 +0400

    Refinement

---
+++ SNAKES/Assets/Heart.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    public Collider2D Heartspawn;
+
+    private void Start()
+    {
+        RandomizePosition();//starts the spawn on a randomized coordiantes
+    }
+
+    public void RandomizePosition()
+    {
+        Bounds bounds = Heartspawn.bounds;
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);//generates random coordinates within set grid
+        x = Mathf.Round(x);
+        y = Mathf.Round(y);//ensures the hearts spawn within a set grid by rounding off the coordinates
+
+        transform.position = new Vector2(x, y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        RandomizePosition();
+    }
+
+}
--- SNAKES/Assets/Snake.cs
+++ SNAKES/Assets/Snake.cs
@@ -4,87 +4,79 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Snake : MonoBehaviour
 {
-    private List<Transform> segments = new List<Transform>();
+    private List<Transform> segments = new List<Transform>();//creates the list for the segments
     public Transform segmentPrefab;
     public Vector2 direction = Vector2.right;
-    public int initialSize = 4;
+    public int initialSize = 8;//initializes the snake saize to 8
 
-    private void Start()
+    private void Start()//Starts the game.
     {
-        ResetState();
+        ResetState();//respawns the snake.
     }
 
     private void Update()
     {
-        // Only allow turning up or down while moving in the x-axis
-        if (direction.x != 0f)
+        
+        if (direction.x != 0f)//handles the movement when the snake is on x axis and allows only up and down movement
         {
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
-                direction = Vector2.up;
+                direction = Vector2.up;//moves the snake up
             } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
-                direction = Vector2.down;
+                direction = Vector2.down;//moves the snake down
             }
         }
-        // Only allow turning left or right while moving in the y-axis
-        else if (direction.y != 0f)
+        
+        else if (direction.y != 0f)//handles the movement when the snake is on x axis and allows only right and left movement
         {
             if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
-                direction = Vector2.right;
+                direction = Vector2.right;//moves the snake right
             } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-                direction = Vector2.left;
+                direction = Vector2.left;//moves the snake left
             }
         }
     }
 
     private void FixedUpdate()
     {
-        // Set each segment's position to be the same as the one it follows. We
-        // must do this in reverse order so the position is set to the previous
-        // position, otherwise they will all be stacked on top of each other.
+        
         for (int i = segments.Count - 1; i > 0; i--) {
-            segments[i].position = segments[i - 1].position;
+            segments[i].position = segments[i - 1].position;//makes the segments always follow the segment before it or the one after it
         }
 
-        // Move the snake in the direction it is facing
-        // Round the values to ensure it aligns to the grid
         float x = Mathf.Round(transform.position.x) + direction.x;
-        float y = Mathf.Round(transform.position.y) + direction.y;
+        float y = Mathf.Round(transform.position.y) + direction.y;//ensures that the snake moves within a set grid by rounding off the coordinates
 
-        transform.position = new Vector2(x, y);
+        transform.position = new Vector2(x, y);//Makes the snake move in the direction it is facing
     }
 
-    public void Grow()
+    public void Grow()//handles when the snake grows a segment
     {
-        Transform segment = Instantiate(segmentPrefab);
+        Transform segment = Instantiate(segmentPrefab);//uses the prefab as the template for the segment
         segment.position = segments[segments.Count - 1].position;
-        segments.Add(segment);
+        segments.Add(segment);//adds a segment to the snake
     }
 
-    public void ResetState()
+    public void ResetState()//resets the game
     {
         direction = Vector2.right;
         transform.position = Vector3.zero;
-
-        // Start at 1 to skip destroying the head
         for (int i = 1; i < segments.Count; i++) {
             Destroy(segments[i].gameObject);
         }
-
-        // Clear the list but add back this as the head
         segments.Clear();
         segments.Add(transform);
 
-        // -1 since the head is already in the list
         for (int i = 0; i < initialSize - 1; i++) {
             Grow();
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)//handles when the snake hits another game objects collider box
     {
-        if (other.gameObject.CompareTag("Food")) {
+        if (other.gameObject.CompareTag("Heart")) {//activates growth of segment when the snake "eats" a Heart
             Grow();
-        } else if (other.gameObject.CompareTag("Obstacle")) {
+        } else if (other.gameObject.CompareTag("Wall")) { 
+            //activates Resetstate to simulate death when the snake "hits" any gameobject tagged Wall (walls and the snakes body itself)
             ResetState();
         }
     }
